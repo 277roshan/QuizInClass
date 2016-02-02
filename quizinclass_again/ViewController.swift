cd8161709eb1bbc40497615231878e81d2ad1e1f
Roshan Thapaliya <gorkhali@roshans-macbook-pro.local>
2016-02-02 15:10:11 -0500

    Initial Commit

---
+++ quizinclass_again/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quizinclass_again
+//
+//  Created by Roshan Thapaliya on 2/2/16.
+//  Copyright © 2016 Roshan Thapaliya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
